b0a38afb6e4f09f6673a9c0728751b5e17600db8
TheGreenBeaver <leonkhvatskin@gmail.com>
2020-10-31 14:37:44 +0300

    task completed

--- app/src/main/java/com/example/nav/ContainerActivity.kt
+++ app/src/main/java/com/example/nav/ContainerActivity.kt
@@ -2,10 +2,20 @@ package com.example.nav
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.container_activity)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_navigation) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
+
     }
 }
